27a69129a8cfd1d3f3cdab6fb5b5e15eaedc5835
JohnWickCP <phonk63utc@gmail.com>
2024-09-02 16:54:57 +0700

    update

--- CeasarEncryptionAlg.cpp
+++ CeasarEncryptionAlg.cpp
@@ -2,17 +2,14 @@
 #include <string>
 using namespace std;
 
-//test
 string encryptCaesar(string text, int key) {
     string result = "";
 
     for (int i = 0; i < text.length(); i++) {
         char c = text[i];
-        // Mã hóa chữ thường
         if (islower(c)) {
             c = (c - 'a' + key) % 26 + 'a';
         }
-        // Mã hóa chữ hoa
         else if (isupper(c)) {
             c = (c - 'A' + key) % 26 + 'A';
         }
@@ -21,29 +18,12 @@ string encryptCaesar(string text, int key) {
     return result;
 }
 
-// Hàm giải mã Caesar
-string decryptCaesar(string text, int key) {
-    return encryptCaesar(text, 26 - key); 
-}
 int main() {
-    string text;
-    int key;
+    string text = "LOVEISBLINDLOVEI";
+    int key = 21;
 
-    // Nhập chuỗi cần mã hóa hoặc giải mã
-    cout << "Nhap text: ";
-    getline(cin, text);
-
-    // Nhập giá trị key
-    cout << "Nhap key: ";
-    cin >> key;
-
-    // Mã hóa văn bản
     string encryptedText = encryptCaesar(text, key);
     cout << "En: " << encryptedText << endl;
-
-    // Giải mã văn bản
-    string decryptedText = decryptCaesar(encryptedText, key);
-    cout << "De: " << decryptedText << endl;
 
     return 0;
 }
