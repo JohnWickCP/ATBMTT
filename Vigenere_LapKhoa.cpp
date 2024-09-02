30facaa428e889570a8d2a138c8f8fb235524f78
JohnWickCP <phonk63utc@gmail.com>
2024-09-02 17:10:37 +0700

    update

---
+++ Vigenere_LapKhoa.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string generateKey(string plaintext, string key) {
+
+    string repeatedKey = key;
+    while (repeatedKey.length() < plaintext.length()) {
+        repeatedKey += key; // Lặp lại khóa
+    }
+    return repeatedKey.substr(0, plaintext.length());
+}
+
+string vigenereEncrypt(string plaintext, string key) {
+
+    string ciphertext;
+    for (size_t i = 0; i < plaintext.length(); i++) {
+
+        char plainChar = plaintext[i];
+        char keyChar = key[i];
+
+        char encryptedChar = ((plainChar - 'A') + (keyChar - 'A')) % 26 + 'A';
+        ciphertext += encryptedChar;
+    }
+    return ciphertext;
+}
+
+int main() {
+    string plaintext = "MONEYISAGOODSER";
+    string key = "EVERYONE";
+
+    // Chuyển plaintext thành chữ hoa
+    for (char &ch : plaintext) {
+        ch = toupper(ch);
+    }
+     // Chuyển khóa thành chữ hoa
+    for (char &ch : key) {
+        ch = toupper(ch);
+    }
+
+    string repeatedKey = generateKey(plaintext, key);
+    string ciphertext = vigenereEncrypt(plaintext, repeatedKey);
+
+    cout << "Ciphertext: " << ciphertext << endl;
+
+    return 0;
+}
